1a2b31d9361fe6e978c2dd5de05d785412ae594d
brunomjanuario <bruno.januario1998@gmail.com>
2024-07-09 19:30:11 +0100

    feat: add training and canvas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { TeamsComponent } from './teams/teams.component';
+import { TrainingComponent } from './training/training.component';
 
 export const routes: Routes = [
     { path: 'teams', component: TeamsComponent },
+    { path: 'training', component: TrainingComponent }
 ];
