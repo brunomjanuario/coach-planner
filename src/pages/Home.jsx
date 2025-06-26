8816225b17c7ef48736bac93941da29e89825a8e
Bruno Januário <bruno.januario1998@gmail.com>
2025-06-26 16:21:10 +0100

    feat: chnage home screen

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,12 @@
 export default function Home() {
   return (
-    <div>
-      <h1>Home</h1>
+    <div className="grid grid-cols-3 grid-rows-2 gap-10 p-5 w-full">
+      <div className="w-full border px-3 py-2 rounded-2xl">Teams</div>
+      <div className="w-full border px-3 py-2 rounded-2xl">Training</div>
+      <div className="w-full border px-3 py-2 rounded-2xl">Games</div>
+      <div className="w-full border px-3 py-2 rounded-2xl">Most Goals</div>
+      <div className="w-full border px-3 py-2 rounded-2xl">Most Games</div>
+      <div className="w-full border px-3 py-2 rounded-2xl">Most Cards</div>
     </div>
   );
 }
